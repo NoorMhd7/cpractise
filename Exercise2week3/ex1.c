dde1f83fb0257e594b908c033a120163b868b02a
NoorMhd7 <146771080+NoorMhd7@users.noreply.github.com>
2023-10-20 16:58:46 +0000

    week3 friday

---
+++ Exercise2week3/ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+float circle_area (float r)
+{
+return( 3.14 * r * r);
+}
+int main (){
+
+float x = circle_area(2);
+printf("Your area of circle of your raduis you have chosen  is %f", x);
+
+
+
+}
---
+++ Exercise2week3/ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+float sphere = (float r)
+{
+return((4 * 3.14 * r * r * r) / 3);
+}
+
+int main () {
+float s = sphere(4);
+printf("The voulume of sphere is %f: \n ",  s) 
+
+
+}
